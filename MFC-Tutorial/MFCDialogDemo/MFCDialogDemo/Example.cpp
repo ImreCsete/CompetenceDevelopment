03324bb7e56b67f75cbc6adc61eac88872dc8366
ImreCsete <31795867+ImreCsete@users.noreply.github.com>
2020-10-19 03:39:02 +0200

    Finished Dialog Box tutorial

--- MFC-Tutorial/MFCDialogDemo/MFCDialogDemo/Example.cpp
+++ MFC-Tutorial/MFCDialogDemo/MFCDialogDemo/Example.cpp
@@ -1,6 +1,37 @@
 #include <afxwin.h>
+#include "resource.h"
 
 class CExample : public CWinApp {
 public:
     BOOL InitInstance();
 };
+
+// Create dialog box by deriving from CDialog, implement default constructor/destructor
+
+class CExampleDlg : public CDialog {
+public:
+    enum { IDD = IDD_EXAMPLE_DLG };
+
+    CExampleDlg();
+    ~CExampleDlg();
+};
+
+CExampleDlg::CExampleDlg() :CDialog(CExampleDlg::IDD) {
+
+}
+
+CExampleDlg::~CExampleDlg() {
+
+}
+
+/* 1. Instantiate dialog box
+*  2. Set box to modal
+*/
+
+BOOL CExample::InitInstance() {
+    CExampleDlg myDlg;
+    m_pMainWnd = &myDlg;
+    myDlg.DoModal();
+    return TRUE;
+}
+CExample MyApp;
